ee129438f44412c746670f54ade1f5e15f5f2dc2
kkunchae <kkunchae@gmail.com>
2022-01-24 20:10:37 -0500

    Added Player Dice Roll

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Dice_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerDiceNum;
+            
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to the Dice Game! Best out of 10 wins! Good luck!");
+
+            //Game Loop
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice!");
+
+                Console.ReadKey();
+
+                //Returns a number between 1 and 6
+                playerDiceNum = random.Next(1,7);
+                Console.WriteLine("You rolled a " + playerDiceNum);
+
+            }
+
+            
+        }
+    }
+}
