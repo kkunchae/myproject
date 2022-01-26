1d2c104b553c3a27f659c36d8784269581bb4ea8
kkunchae <kkunchae@gmail.com>
2022-01-25 21:39:54 -0500

    Added GetDocument and GetBookLinks

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using HtmlAgilityPack;
+using System.Collections.Generic;
 
 class WebScraper
 {
@@ -7,11 +8,32 @@ class WebScraper
     static void Main(string[] args)
     {
         string url = "https://books.toscrape.com/catalogue/category/books/food-and-drink_33/index.html";
+        var links = GetBookLinks(url);
 
-        var web = new HtmlWeb();
-        HtmlDocument doc = web.Load(url);
-        Console.WriteLine(doc.DocumentNode.InnerHtml);
-
+        Console.WriteLine(GetDocument(url).DocumentNode.InnerHtml);
     }
 
+
+    static List<string> GetBookLinks(string url)
+    {
+        var doc = GetDocument(url);
+        var linkNodes = doc.DocumentNode.SelectNodes("//h3/a");
+
+        var baseUri = new Uri(url);
+        var links = new List<string>();
+        foreach (var node in linkNodes)
+        {
+            var link = node.Attributes["href"].Value;
+            link = new Uri(baseUri, link).AbsolutePath;
+            links.Add(link);
+        }
+        return links;
+    }
+
+    static HtmlDocument GetDocument(string url)
+    {
+        var web = new HtmlWeb();
+        HtmlDocument doc = web.Load(url);
+        return doc;
+    }
 }
