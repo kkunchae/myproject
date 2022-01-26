1304127972bfc0cc569709022981dfa6ebd33ee3
kkunchae <kkunchae@gmail.com>
2022-01-25 22:48:57 -0500

    moved book.cs to the correct space

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -11,8 +11,24 @@ namespace scraper
         {
             string url = "https://books.toscrape.com/catalogue/category/books/food-and-drink_33/index.html";
             var links = GetBookLinks(url);
+            List<Books> books = GetBooks();
 
-            Console.WriteLine(GetDocument(url).DocumentNode.InnerHtml);
+        }
+
+        //Retrieve book information
+        static List<Book> GetBooks(List<string> links)
+        {
+            var books = new List<Book>();
+            foreach(var link in links)
+            {
+                var doc = GetDocument(link);
+                var book = new Book();
+                book.Title = doc.DocumentNode.SelectSingleNode("//h1").InnerText;
+                var xpath = "//*[@class=\"col-sm-6 product_main\"]/*[@class=\"price_color\"]";
+                book.Price = doc.DocumentNode.SelectSingleNode(xpath).InnerText;
+                books.Add(book);
+            }
+            return books;
         }
 
         //Retrieves all the books on the page
---
+++ WebScraper/book.cs
@@ -0,0 +1,10 @@
+namespace scraper
+{
+    class Book
+    {
+        public string Title { get; set;}
+        public string Price { get; set;}
+        public string Avl { get; set;}
+    }
+
+}
