66ee45799f7226ba04546f2afbd7727e5ac049d9
kkunchae <kkunchae@gmail.com>
2022-01-25 21:07:49 -0500

    Testing HtmlAgilityPack

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using HtmlAgilityPack;
+
+class WebScraper
+{
+
+    static void Main(string[] args)
+    {
+        string url = "https://books.toscrape.com/catalogue/category/books/food-and-drink_33/index.html";
+
+        var web = new HtmlWeb();
+        HtmlDocument doc = web.Load(url);
+        Console.WriteLine(doc.DocumentNode.InnerHtml);
+
+    }
+
+}
